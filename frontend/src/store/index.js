3e80cfd435cacab57d3ce4538221f218eadec932
a925115576-code <a925115576@gmail.com>
2026-06-24 18:25:12 +0800

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -97,7 +97,7 @@ export const useGlobalState = createGlobalState(
         const globalTabplacement = useStorage('globalTabplacement', 'top');
         const useSideMargin = useStorage('useSideMargin', true);
         const useUTCDate = useStorage('useUTCDate', false);
-        const autoRefresh = useStorage('autoRefresh', ture);
+        const autoRefresh = useStorage('autoRefresh', true);
         const configAutoRefreshInterval = useStorage("configAutoRefreshInterval", 5);
         const userOpenSettings = ref({
             fetched: false,
